257c41c3e8032f0a7c65bc67dd7d9cfb838d6c11
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2024-11-05 22:58:50 +0900

    Refactor FrameGenerator

--- VCamSampleSource/FrameGenerator.cpp
+++ VCamSampleSource/FrameGenerator.cpp
@@ -32,32 +32,19 @@ HRESULT FrameGenerator::SetupD3D11Device() {
 
 	RETURN_IF_FAILED(D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE,
 		nullptr, createDeviceFlags, d3dFeatures, 1, D3D11_SDK_VERSION,
-		device.put(), nullptr, _textureDeviceContext.put()));
+		device.put(), nullptr, _dxDeviceContext.put()));
 
-	_textureDeviceContext->QueryInterface(IID_PPV_ARGS(_deviceMutex.put()));
-	_deviceMutex->SetMultithreadProtected(true);
+	_dxDeviceContext->QueryInterface(IID_PPV_ARGS(_dxDeviceMutex.put()));
+	_dxDeviceMutex->SetMultithreadProtected(true);
 
 	_dxgiManager->ResetDevice(device.get(), resetToken);
 
-	device->QueryInterface(IID_PPV_ARGS(_textureDevice.put()));
+	device->QueryInterface(IID_PPV_ARGS(_dxDevice.put()));
 
 	return S_OK;
 }
 
-HRESULT FrameGenerator::CreateRenderTargetResources()
-{
-	assert(_renderTarget);
-	RETURN_IF_FAILED(_renderTarget->CreateSolidColorBrush(D2D1::ColorF(1, 1, 1, 1), &_whiteBrush));
-
-	RETURN_IF_FAILED(DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), (IUnknown**)&_dwrite));
-	RETURN_IF_FAILED(_dwrite->CreateTextFormat(L"Segoe UI", nullptr, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL, 40, L"", &_textFormat));
-	RETURN_IF_FAILED(_textFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER));
-	RETURN_IF_FAILED(_textFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER));
-	return S_OK;
-}
-
-// CPUからアクセス可能なテクスチャを作成し、
-// ハンドルから共有されたテクスチャを取得。
+// キャプチャウィンドウの共有テクスチャをハンドルから取得。
 HRESULT FrameGenerator::CreateSharedCaptureWindowTexture()
 {
 	if (_sharedCaptureWindowTexture != nullptr)
@@ -65,18 +52,20 @@ HRESULT FrameGenerator::CreateSharedCaptureWindowTexture()
 		return S_OK;
 	}
 
-	RETURN_IF_FAILED(_textureDevice->OpenSharedResourceByName(
+	RETURN_IF_FAILED(_dxDevice->OpenSharedResourceByName(
 		TEXT("Global\\NM_Capture_Window"), DXGI_SHARED_RESOURCE_READ, 
 		IID_PPV_ARGS(_sharedCaptureWindowTexture.put())));
 
-	D3D11_TEXTURE2D_DESC textureDesc;
-	_sharedCaptureWindowTexture->GetDesc(&textureDesc);
-	_captureTextureWidth = textureDesc.Width;
-	_captureTextureHeight = textureDesc.Height;
+	D3D11_TEXTURE2D_DESC sharedTextureDesc;
+	_sharedCaptureWindowTexture->GetDesc(&sharedTextureDesc);
+	_captureTextureWidth = sharedTextureDesc.Width;
+	_captureTextureHeight = sharedTextureDesc.Height;
 
 	return S_OK;
 }
 
+// キャプチャウィンドウのサイズなどパラメタを格納したの共有テクスチャをハンドルから取得。
+// CPU上でデータを読みだすためのテクスチャの作成。
 HRESULT FrameGenerator::CreateSharedParamsTexture()
 {
 	if (_sharedParamsTexture != nullptr)
@@ -84,17 +73,16 @@ HRESULT FrameGenerator::CreateSharedParamsTexture()
 		return S_OK;
 	}
 
-	D3D11_TEXTURE2D_DESC bufferTextureDesc;
-
-	RETURN_IF_FAILED(_textureDevice->OpenSharedResourceByName(
+	RETURN_IF_FAILED(_dxDevice->OpenSharedResourceByName(
 		TEXT("Global\\NM_Capture_Window_Params"), DXGI_SHARED_RESOURCE_READ, 
 		IID_PPV_ARGS(_sharedParamsTexture.put())));
 
-	D3D11_TEXTURE2D_DESC textureDesc;
-	_sharedParamsTexture->GetDesc(&textureDesc);
-	UINT sharedParamsTextureWidth = textureDesc.Width;
-	UINT sharedParamsTextureHeight = textureDesc.Height;
+	D3D11_TEXTURE2D_DESC sharedTextureDesc;
+	_sharedParamsTexture->GetDesc(&sharedTextureDesc);
+	UINT sharedParamsTextureWidth = sharedTextureDesc.Width;
+	UINT sharedParamsTextureHeight = sharedTextureDesc.Height;
 
+	D3D11_TEXTURE2D_DESC bufferTextureDesc;
 	bufferTextureDesc.Width = sharedParamsTextureWidth;
 	bufferTextureDesc.Height = sharedParamsTextureHeight;
 	bufferTextureDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
@@ -106,39 +94,41 @@ HRESULT FrameGenerator::CreateSharedParamsTexture()
 	bufferTextureDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
 	bufferTextureDesc.MiscFlags = 0;
 	bufferTextureDesc.Usage = D3D11_USAGE_STAGING;
-	RETURN_IF_FAILED(_textureDevice->CreateTexture2D(&bufferTextureDesc, 0, 
+	RETURN_IF_FAILED(_dxDevice->CreateTexture2D(&bufferTextureDesc, 0, 
 		_cpuParamsTexture.put()));
 
 	return S_OK;
 }
 
+// _renderTextureへのオフスクリーンレンダリングの準備
 HRESULT FrameGenerator::SetupOffscreenRendering() {
 	DXGI_FORMAT dxgiFormat = DXGI_FORMAT_B8G8R8A8_UNORM;
-	CD3D11_TEXTURE2D_DESC desc
-	(
-		dxgiFormat,
-		_width,
-		_height,
-		1,
-		1,
-		D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET,
-		D3D11_USAGE_DEFAULT,
-		0,
-		1
-	);
-	RETURN_IF_FAILED(_textureDevice->CreateTexture2D(&desc, nullptr, _texture.put()));
+	CD3D11_TEXTURE2D_DESC desc;
+	desc.Width = _width;
+	desc.Height = _height;
+	desc.Format = dxgiFormat;
+	desc.ArraySize = 1;
+	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+	desc.MipLevels = 1;
+	desc.SampleDesc.Count = 1;
+	desc.SampleDesc.Quality = 0;
+	desc.CPUAccessFlags = 0;
+	desc.MiscFlags = 0;
+	desc.Usage = D3D11_USAGE_DEFAULT;
+	
+	RETURN_IF_FAILED(_dxDevice->CreateTexture2D(&desc, nullptr, _renderTexture.put()));
 
 	CD3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc(D3D11_RTV_DIMENSION_TEXTURE2D, dxgiFormat);
-	RETURN_IF_FAILED(_textureDevice->CreateRenderTargetView(_texture.get(),
+	RETURN_IF_FAILED(_dxDevice->CreateRenderTargetView(_renderTexture.get(),
 		&renderTargetViewDesc, _renderTargetView.put()));
 
 	CD3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc(D3D11_SRV_DIMENSION_TEXTURE2D, dxgiFormat);
-	RETURN_IF_FAILED(_textureDevice->CreateShaderResourceView(_sharedCaptureWindowTexture.get(),
+	RETURN_IF_FAILED(_dxDevice->CreateShaderResourceView(_sharedCaptureWindowTexture.get(),
 		&shaderResourceViewDesc, _shaderResourceView.put()));
 
-	_textureDeviceContext->OMSetRenderTargets(1, _renderTargetView.addressof(), nullptr);
+	_dxDeviceContext->OMSetRenderTargets(1, _renderTargetView.addressof(), nullptr);
 	D3D11_VIEWPORT vp = { 0.0f, 0.0f, (float)_width, (float)_height, 0.0f, 1.0f };
-	_textureDeviceContext->RSSetViewports(1, &vp);
+	_dxDeviceContext->RSSetViewports(1, &vp);
 
 	size_t hlslSize = std::strlen(hlslCode);
 	wil::com_ptr_t<ID3DBlob> compiledVS;
@@ -149,10 +139,10 @@ HRESULT FrameGenerator::SetupOffscreenRendering() {
 	RETURN_IF_FAILED(D3DCompile(hlslCode, hlslSize, nullptr, nullptr, nullptr,
 		"PS", "ps_5_0", 0, 0, compiledPS.put(), nullptr));
 
-	RETURN_IF_FAILED(_textureDevice->CreateVertexShader(compiledVS->GetBufferPointer(),
+	RETURN_IF_FAILED(_dxDevice->CreateVertexShader(compiledVS->GetBufferPointer(),
 		compiledVS->GetBufferSize(), nullptr, _spriteVS.put()));
 
-	RETURN_IF_FAILED(_textureDevice->CreatePixelShader(compiledPS->GetBufferPointer(),
+	RETURN_IF_FAILED(_dxDevice->CreatePixelShader(compiledPS->GetBufferPointer(),
 		compiledPS->GetBufferSize(), nullptr, _spritePS.put()));
 
 	D3D11_INPUT_ELEMENT_DESC layout[2] = {
@@ -160,15 +150,23 @@ HRESULT FrameGenerator::SetupOffscreenRendering() {
 		{"TEXUV", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
 	};
 
-	RETURN_IF_FAILED(_textureDevice->CreateInputLayout(layout, 2, compiledVS->GetBufferPointer(),
+	RETURN_IF_FAILED(_dxDevice->CreateInputLayout(layout, 2, compiledVS->GetBufferPointer(),
 		compiledVS->GetBufferSize(), _spriteInputLayout.put()));
+
+	_vbDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	_vbDesc.ByteWidth = sizeof(VertexType) * 4;
+	_vbDesc.MiscFlags = 0;
+	_vbDesc.StructureByteStride = 0;
+	_vbDesc.Usage = D3D11_USAGE_DEFAULT;
+	_vbDesc.CPUAccessFlags = 0;
 
 	return S_OK;
 }
 
+// NV12フォーマットのコンバータ (IMFTransform) 作成
 HRESULT FrameGenerator::SetupNV12Converter() {
 	// create GPU RGB => NV12 converter
-	RETURN_IF_FAILED(CoCreateInstance(CLSID_VideoProcessorMFT, nullptr, CLSCTX_ALL, IID_PPV_ARGS(&_converter)));
+	RETURN_IF_FAILED(CoCreateInstance(CLSID_VideoProcessorMFT, nullptr, CLSCTX_ALL, IID_PPV_ARGS(_converter.put())));
 
 	wil::com_ptr_nothrow<IMFAttributes> atts;
 	RETURN_IF_FAILED(_converter->GetAttributes(&atts));
@@ -215,8 +213,6 @@ HRESULT FrameGenerator::EnsureRenderTarget(UINT width, UINT height)
 		RETURN_IF_FAILED(SetupNV12Converter());
 	}
 
-	_prevTime = MFGetSystemTime();
-	_frame = 0;
 	return S_OK;
 }
 
@@ -227,14 +223,8 @@ void FrameGenerator::DrawSharedCaptureWindow()
 		return;
 	}
 
-	float color[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
-	_textureDeviceContext->ClearRenderTargetView(_renderTargetView.get(), color);
-
-	struct VertexType
-	{
-		DirectX::XMFLOAT3 Pos;
-		DirectX::XMFLOAT2 Tex;
-	};
+	float color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	_dxDeviceContext->ClearRenderTargetView(_renderTargetView.get(), color);
 
 	float xPosRate = 1.0f;
 	float yPosRate = 1.0f;
@@ -261,37 +251,28 @@ void FrameGenerator::DrawSharedCaptureWindow()
 		{{xPosRate, -yPosRate, 0}, {widthTextureRate, heightTextureRate}}
 	};
 
-	D3D11_BUFFER_DESC vbDesc = {};
-	vbDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	vbDesc.ByteWidth = sizeof(v);
-	vbDesc.MiscFlags = 0;
-	vbDesc.StructureByteStride = 0;
-	vbDesc.Usage = D3D11_USAGE_DEFAULT;
-	vbDesc.CPUAccessFlags = 0;
-
 	wil::com_ptr_nothrow<ID3D11Buffer> vb;
 	D3D11_SUBRESOURCE_DATA initData = {
-		&v[0], sizeof(v), 0
+		v, sizeof(v), 0
 	};
 
-	_textureDevice->CreateBuffer(&vbDesc, &initData, &vb);
+	_dxDevice->CreateBuffer(&_vbDesc, &initData, &vb);
 
 	UINT stride = sizeof(VertexType);
 	UINT offset = 0;
-	_textureDeviceContext->IASetVertexBuffers(0, 1, vb.addressof(), &stride, &offset);
-	_textureDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
-	_textureDeviceContext->VSSetShader(_spriteVS.get(), 0, 0);
-	_textureDeviceContext->PSSetShader(_spritePS.get(), 0, 0);
-	_textureDeviceContext->IASetInputLayout(_spriteInputLayout.get());
+	_dxDeviceContext->IASetVertexBuffers(0, 1, vb.addressof(), &stride, &offset);
+	_dxDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	_dxDeviceContext->VSSetShader(_spriteVS.get(), 0, 0);
+	_dxDeviceContext->PSSetShader(_spritePS.get(), 0, 0);
+	_dxDeviceContext->IASetInputLayout(_spriteInputLayout.get());
 
-	com_ptr<IDXGIKeyedMutex> mutexCapture;
-	_sharedCaptureWindowTexture.as(mutexCapture);
-	mutexCapture->AcquireSync(MUTEX_KEY, INFINITE);
-	_textureDeviceContext->PSSetShaderResources(0, 1, _shaderResourceView.addressof());
-	_textureDeviceContext->Draw(4, 0);
-	_textureDeviceContext->Flush();
-	mutexCapture->ReleaseSync(MUTEX_KEY);
-
+	wil::com_ptr_nothrow<IDXGIKeyedMutex> mutex;
+	_sharedCaptureWindowTexture->QueryInterface(IID_PPV_ARGS(mutex.put()));
+	mutex->AcquireSync(MUTEX_KEY, INFINITE);
+	_dxDeviceContext->PSSetShaderResources(0, 1, _shaderResourceView.addressof());
+	_dxDeviceContext->Draw(4, 0);
+	_dxDeviceContext->Flush();
+	mutex->ReleaseSync(MUTEX_KEY);
 }
 
 void FrameGenerator::GetParamsFromTexture()
@@ -300,13 +281,14 @@ void FrameGenerator::GetParamsFromTexture()
 	{
 		return;
 	}
-	com_ptr<IDXGIKeyedMutex> mutex;
-	_sharedParamsTexture.as(mutex);
+
+	wil::com_ptr_nothrow<IDXGIKeyedMutex> mutex;
+	_sharedParamsTexture->QueryInterface(IID_PPV_ARGS(mutex.put()));
 	mutex->AcquireSync(MUTEX_KEY, INFINITE);
-	_textureDeviceContext->CopyResource(_cpuParamsTexture.get(), _sharedParamsTexture.get());
+	_dxDeviceContext->CopyResource(_cpuParamsTexture.get(), _sharedParamsTexture.get());
 	mutex->ReleaseSync(MUTEX_KEY);
 
-	com_ptr<IDXGISurface> dxgiSurface;
+	wil::com_ptr_nothrow<IDXGISurface> dxgiSurface;
 	_cpuParamsTexture->QueryInterface(IID_PPV_ARGS(dxgiSurface.put()));
 
 	DXGI_MAPPED_RECT mapFromTexture;
@@ -325,65 +307,19 @@ HRESULT FrameGenerator::Generate(IMFSample* sample, REFGUID format, IMFSample** 
 	RETURN_HR_IF_NULL(E_POINTER, outSample);
 	*outSample = nullptr;
 
-	_deviceMutex->Enter();
+	_dxDeviceMutex->Enter();
 	GetParamsFromTexture();
 
 	DrawSharedCaptureWindow();
-	_deviceMutex->Leave();
-	// render something on image common to CPU & GPU
-	/*if (_renderTarget)
-	{
-		_renderTarget->BeginDraw();
-		_renderTarget->Clear(D2D1::ColorF(0, 0, 0, 1));
+	_dxDeviceMutex->Leave();
 
-		DrawSharedCaptureWindow();
-
-		// draw resolution at center
-		// note: we could optimize here and compute layout only once if text doesn't change (depending on the font, etc.)
-#ifdef _DEBUG
-		if (_textFormat && _dwrite && _whiteBrush)
-		{
-			wchar_t text[127];
-			wchar_t fmt[15];
-			if (format == MFVideoFormat_NV12)
-			{
-				lstrcpy(fmt, L"NV12 (GPU)");
-			}
-			else
-			{
-				lstrcpy(fmt, L"RGB32 (GPU)");
-			}
-
-#define FRAMES_FOR_FPS 60 // number of frames to wait to compute fps from last measure
-#define NS_PER_MS 10000
-#define MS_PER_S 1000
-
-			if (!_fps || !(_frame % FRAMES_FOR_FPS))
-			{
-				auto time = MFGetSystemTime();
-				_fps = (UINT)(MS_PER_S * NS_PER_MS * FRAMES_FOR_FPS / (time - _prevTime));
-				_prevTime = time;
-			}
-
-			auto len = wsprintf(text, L"Format: %s\nFrame#: %I64i\nFps: %u\nResolution: %u x %u", fmt, _frame, _fps, _width, _height);
-
-			wil::com_ptr_nothrow<IDWriteTextLayout> layout;
-			RETURN_IF_FAILED(_dwrite->CreateTextLayout(text, len, _textFormat.get(), (FLOAT)_width, (FLOAT)_height, &layout));
-
-			_renderTarget->DrawTextLayout(D2D1::Point2F(0, 0), layout.get(), _whiteBrush.get());
-		}
-#endif
-		_renderTarget->EndDraw();
-	}*/
-
-	// build a sample using either D3D/DXGI (GPU) or WIC (CPU)
 	wil::com_ptr_nothrow<IMFMediaBuffer> mediaBuffer;
 
 	// remove all existing buffers
 	RETURN_IF_FAILED(sample->RemoveAllBuffers());
 
 	// create a buffer from this and add to sample
-	RETURN_IF_FAILED(MFCreateDXGISurfaceBuffer(__uuidof(ID3D11Texture2D), _texture.get(), 0, 0, &mediaBuffer));
+	RETURN_IF_FAILED(MFCreateDXGISurfaceBuffer(__uuidof(ID3D11Texture2D), _renderTexture.get(), 0, 0, &mediaBuffer));
 	RETURN_IF_FAILED(sample->AddBuffer(mediaBuffer.get()));
 
 	// if we're on GPU & format is not RGB, convert using GPU
@@ -404,6 +340,5 @@ HRESULT FrameGenerator::Generate(IMFSample* sample, REFGUID format, IMFSample** 
 		*outSample = sample;
 	}
 
-	_frame++;
 	return S_OK;
 }
--- VCamSampleSource/FrameGenerator.h
+++ VCamSampleSource/FrameGenerator.h
@@ -3,46 +3,41 @@
 #include <d3d11_4.h>
 #include <dxgi1_2.h>
 
-using namespace winrt;
-
 #define MUTEX_KEY 5556
 
 class FrameGenerator
 {
+	struct VertexType
+	{
+		DirectX::XMFLOAT3 Pos;
+		DirectX::XMFLOAT2 Tex;
+	};
+
 	UINT _width;
 	UINT _height;
-	ULONGLONG _frame;
-	MFTIME _prevTime;
-	UINT _fps;
-	wil::com_ptr_nothrow<ID3D11Texture2D> _texture;
-	wil::com_ptr_nothrow<ID2D1RenderTarget> _renderTarget;
-	wil::com_ptr_nothrow<ID2D1SolidColorBrush> _whiteBrush;
-	wil::com_ptr_nothrow<IDWriteTextFormat> _textFormat;
-	wil::com_ptr_nothrow<IDWriteFactory> _dwrite;
-	wil::com_ptr_nothrow<IMFTransform> _converter;
-	wil::com_ptr_nothrow<IWICBitmap> _bitmap;
+
 	wil::com_ptr_nothrow<IMFDXGIDeviceManager> _dxgiManager;
+	wil::com_ptr_nothrow<ID3D11Device1> _dxDevice;
+	wil::com_ptr_nothrow<ID3D11DeviceContext> _dxDeviceContext;
+	wil::com_ptr_nothrow<ID3D11Multithread> _dxDeviceMutex;
+	wil::com_ptr_nothrow<ID3D11Texture2D> _renderTexture;
+	wil::com_ptr_nothrow<IMFTransform> _converter;
+	
 	wil::com_ptr_nothrow<ID3D11RenderTargetView> _renderTargetView;
 	wil::com_ptr_nothrow<ID3D11ShaderResourceView> _shaderResourceView;
 	wil::com_ptr_nothrow<ID3D11VertexShader> _spriteVS;
 	wil::com_ptr_nothrow<ID3D11PixelShader> _spritePS;
 	wil::com_ptr_nothrow<ID3D11InputLayout> _spriteInputLayout;
+	D3D11_BUFFER_DESC _vbDesc;
 
-	HRESULT CreateRenderTargetResources();
-
-	// 共有テクスチャのために追加した部分
-	com_ptr<ID3D11Device1> _textureDevice;
-	com_ptr<ID3D11DeviceContext> _textureDeviceContext;
-	com_ptr<ID3D11Multithread> _deviceMutex;
-
-	com_ptr<ID3D11Texture2D> _sharedCaptureWindowTexture;
+	wil::com_ptr_nothrow<ID3D11Texture2D> _sharedCaptureWindowTexture;
 	HANDLE _sharedCaptureWindowHandle;
 	int _captureTextureWidth;
 	int _captureTextureHeight;
 	
-	com_ptr<ID3D11Texture2D> _sharedParamsTexture;
+	wil::com_ptr_nothrow<ID3D11Texture2D> _sharedParamsTexture;
 	HANDLE _sharedParamsHandle;
-	com_ptr<ID3D11Texture2D> _cpuParamsTexture;
+	wil::com_ptr_nothrow<ID3D11Texture2D> _cpuParamsTexture;
 	int _captureWindowWidth;
 	int _captureWindowHeight;
 
@@ -59,15 +54,11 @@ class FrameGenerator
 	void DrawSharedCaptureWindow();
 
 	void GetParamsFromTexture();
-	// 共有テクスチャのために追加した部分 END
 
 public:
 	FrameGenerator() :
 		_width(0),
 		_height(0),
-		_frame(0),
-		_fps(0),
-		_prevTime(MFGetSystemTime()),
 		_captureTextureWidth(0),
 		_captureTextureHeight(0),
 		_captureWindowWidth(0),
